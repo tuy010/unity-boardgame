f6b1273ec19d68eaaf3e839d249d4189327ef965
tuy010 <104480146+tuy010@users.noreply.github.com>
2023-03-04 21:20:13 +0900

    Update ItemWeapon.cs

--- My project/Assets/Data/Item/Script/ItemWeapon.cs
+++ My project/Assets/Data/Item/Script/ItemWeapon.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Progress;
+//using static UnityEditor.Progress;
 
 [CreateAssetMenu(fileName = "New Weapon", menuName = "New Item/Weapon")]
 public class ItemWeapon : Item
